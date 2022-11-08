a98267688d2181adbe4219b7683178cd7e111706
Walls182 <wasc0144@gmail.com>
2022-11-08 10:37:51 -0500

    1.0.2
    Cambios hechos en clase

--- src/laboratorioexcepciones2/LaboratorioExcepciones2.java
+++ src/laboratorioexcepciones2/LaboratorioExcepciones2.java
@@ -12,28 +12,39 @@ import javax.swing.JOptionPane;
  */
 public class LaboratorioExcepciones2   {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NumberMuchoException {
         try {
+            
             int a = Integer.parseInt(JOptionPane.showInputDialog("ingrese un numero"));
             int b = Integer.parseInt(JOptionPane.showInputDialog("ingrese el numero a dividir"));
+            //Lanzamos nuestra propia excepcion;
+            rango(a,b);
             metodoDivide(a, b);
             
         } catch (ArithmeticException e) {
           
-              System.out.println("No se puede dividir por cero o como puse la excepcion en el metodoDivide no puede ser mayor a 10  " + e);
+              System.out.println("No se puede dividir por cero " + e);
         }catch (NumberFormatException e){
         
              System.out.println("Debe digitar un numero " + e);
         
+        }catch(NumberMuchoException e){
+        
+            System.out.println("Error "+ e
+            );
         }
+        
 
     }
+    public static void rango(int num,int num1)throws NumberMuchoException{
+    if (num>10||num1>10) {
+                throw new NumberMuchoException("El numero tiene un parametro muy alto");
+            }
 
-    public static void metodoDivide(int a, int b) throws NumberFormatException {
+}
+    public static void metodoDivide(int a, int b) throws NumberMuchoException {
         int resultado;
-        if (b>10) {
-             throw new NumberFormatException();
-        }
+       
 
             resultado = a / b;
         System.out.println(resultado);
